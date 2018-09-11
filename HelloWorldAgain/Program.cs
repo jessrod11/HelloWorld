1c0117cc08a3e75b7d6b1435a5fd8e4207bc497f
Stix <jessica.rodgers@gmail.com>
2018-09-10 20:26:59 -0500

    completed first exercise from C# book

--- HelloWorldAgain/Program.cs
+++ HelloWorldAgain/Program.cs
@@ -6,7 +6,36 @@ namespace HelloWorldAgain
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            string userName;
+            int userAge;
+            string favQuote;
+            string quoteBy;
+            string answer;
+
+            Console.WriteLine("Hello User! What is your name?");
+            userName = Console.ReadLine();
+
+            Console.WriteLine("Cool name, {0}! How old are you?", userName);
+            userAge = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("That's interesting! I thought... Nevermind. I love words! What's your favorite quote?");
+            favQuote = Console.ReadLine();
+
+            Console.WriteLine("Nice! Who is that quote by?");
+            quoteBy = Console.ReadLine();
+
+            Console.WriteLine("I think you're pretty cool! Let's see if I remember these things. Your name is {0}. You are {1} years old and {2} by {3} is your favorite quote.  Am I right? (y/n)", userName, userAge, favQuote, quoteBy);
+            answer = Console.ReadLine();
+
+            if (answer == "y")
+            {
+                Console.WriteLine("Yay! Can we be best friends?");
+            } else
+            {
+                Console.WriteLine("Wow... well you're a sucky programmer!");
+            }
+            Console.ReadLine();
+
         }
     }
 }
